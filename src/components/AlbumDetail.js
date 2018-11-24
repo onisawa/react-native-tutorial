65be670ea3ff99748fab8ec77a400be1ecfaffdb
Visava <onisawavisava@gmail.com>
2018-11-24 22:09:00 +0700

    preparing album card component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Card from './Card'
+
+const AlbumDetail = ({ album }) => {
+  const { title } = album;
+  return (
+    <Card>
+      <Text>{title}</Text>
+    </Card>
+  );
+};
+
+export default AlbumDetail;
