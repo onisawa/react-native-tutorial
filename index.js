b531466fc21496d667a86b36524f2af380da1e50
Visava <onisawavisava@gmail.com>
2018-11-25 18:57:14 +0700

    refactor index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
-
-import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList';
-import text from './src/constant/text.json';
-
-const App = () => (
-  <View style={{ flex: 1 }}>
-    <Header headerText={text.album_list_header} />
-    <AlbumList />
-  </View>
-);
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
 AppRegistry.registerComponent('albumsExample', () => App);
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import Header from './components/Header';
+import AlbumList from './components/AlbumList';
+import text from './constant/text.json';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={text.album_list_header} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
+
+export default App;
